3c8c12907693200c33b8850ca8bc969c210a69b4
G&G={AKG} <Akhileshji831@gmail.com>
2023-11-16 19:47:09 +0530

    Update deployjobBuilder.groovy

--- vars/deployjobBuilder.groovy
+++ vars/deployjobBuilder.groovy
@@ -194,7 +194,7 @@ def call(Map params) {
             definition {
               cps {
                   script('''library 'ci-libs'
-                              selfDeployer(repo:'git@github.com:egovernments/DIGIT-DevOps.git', branch: 'singleinstance', helmDir: 'deploy-as-code/helm')''')
+                              selfDeployer(repo:'git@github.com:beehyv/DIGIT-DevOps-k3d.git', branch: 'kubernetes-1.22-changes', helmDir: 'config-as-code/helm')''')
                   sandbox() 
                 }
             }
